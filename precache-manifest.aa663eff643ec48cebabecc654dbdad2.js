44f7b9000052ba3113984b3afab0a9e8f81417a7
Niall Curtis <CurtisNS@cardiff.ac.uk>
2019-07-07 17:15:03 +0100

    Updates

---
+++ precache-manifest.aa663eff643ec48cebabecc654dbdad2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b023c72f8ed6ab5e50d5633f055d2ac9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d6f7c9cba182f9991ba3",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "06c5297cbba43f77ba53",
+    "url": "/static/js/2.96fb5d64.chunk.js"
+  },
+  {
+    "revision": "d6f7c9cba182f9991ba3",
+    "url": "/static/js/main.57ced37d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0d8f954068db737c9ba7012e19333ca6",
+    "url": "/static/media/niall.0d8f9540.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.054774adbe886ee6e3c29227ef1745b5.js"
+  "/precache-manifest.aa663eff643ec48cebabecc654dbdad2.js"
 );
 
 self.addEventListener('message', (event) => {
